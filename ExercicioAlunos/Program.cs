30c32358a724d5cebd0b0cd7b4b2f3a32083239b
Ágatha Louzada <100931506+agathalouzada@users.noreply.github.com>
2023-03-14 11:02:31 -0300

    Desafio feito

--- ExercicioAlunos/Program.cs
+++ ExercicioAlunos/Program.cs
@@ -3,12 +3,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        {
+        Console.WriteLine("Digite o nome do aluno: ");
         Alunos aluno1 = new Alunos();
-        aluno1.nome = "Guilherme Lene";
-        aluno1.nota1 = 4.3;
-        aluno1.nota2 = 7.8;
+        aluno1.nome = Console.ReadLine();
+        Console.WriteLine("*****************************");
+
+        Console.WriteLine("Digite a primeira nota: ");
+        aluno1.nota1 = double.Parse(Console.ReadLine());
+        Console.WriteLine("Digite a segunda nota: ");
+        aluno1.nota2 = double.Parse(Console.ReadLine());
 
         aluno1.mensagem();
     }
-    
+
+    }
 }
