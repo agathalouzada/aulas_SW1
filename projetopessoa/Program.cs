0862f0b8fecda33dab023459b80520ab3d92e20b
Ágatha Louzada <100931506+agathalouzada@users.noreply.github.com>
2023-03-28 11:05:15 -0300

    Add files via upload

---
+++ projetopessoa/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace projetopessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa(" Ciclano", 45);
+        //p1.InsereNome(" Agatha");
+        //p1.InsereSobreNome(" Louzada");
+        //p1.InsereIdade( 16);
+
+        p1.MostraDados();
+
+    }
+}
