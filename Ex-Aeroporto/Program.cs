9ce1c3d01635c015c898b6da52922f43a00bb6c1
Ágatha Louzada <100931506+agathalouzada@users.noreply.github.com>
2023-04-04 11:52:31 -0300

    Add files via upload

---
+++ Ex-Aeroporto/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex_Aeroporto;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aeroporto a1 = new Aeroporto();
+        a1.AlteraCodigo(1);
+        a1.AlteraEmpresa("Paper Airplanes");
+        //a1.AlteraEntrada("New York");
+        //a1.AlteraSaida("Rússia");
+        a1.MostraDados();
+        
+    }
+}
